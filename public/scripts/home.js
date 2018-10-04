b860e55057c97b754330f6afd21b06d93c0df185
Jesal Chana <jesalchana23@gmail.com>
2018-10-04 13:56:36 +0200

    Added the ability to retrieve from the database and display to html page.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,9 +1,33 @@
 let button = document.getElementById('updateButton');
 
 button.addEventListener('click', function() {
-    alert( "Hello World!" );
-    console.log("Hey");
+    //alert( "Hello World!" );
+    console.log("Update required!!!");
 }, false);
+
+
+
+const building = document.querySelector('#Building');
+
+function renderBuilding(doc) {
+    let li = document.createElement('li');
+    let build = document.createElement('span');
+
+    li.setAttribute('data-id',doc.id);
+    build.textContent = doc.data().Building;
+
+    li.appendChild(build);
+
+    building.appendChild(li);
+}
+
+db.collection('2018').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+
+        renderBuilding(doc);
+        console.log(doc.data().Building);
+    });
+});
 
 
 
